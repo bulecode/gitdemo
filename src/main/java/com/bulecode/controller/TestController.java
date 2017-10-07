f1c6214824f251c8c41da39d6dee31ea249b1100
bulecode <18585862208@163.com>
2017-10-07 20:47:40 +0800

    test controller

---
+++ src/main/java/com/bulecode/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.bulecode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author: buleCode
+ * Date: 2017/10/7
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping
+    public String test1(String a) {
+        return a + "";
+    }
+
+}
